6ec080707b8096f9f30dd594536698c2b5df5bd6
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-28 17:22:26 +0900

    add DownloadButton

--- host/App.js
+++ host/App.js
@@ -8,13 +8,14 @@ import Users from './Users.js'
 import Chart from 'shared/Chart.js'
 import DemandEditor from './DemandEditor.js'
 import MatchingButton from './MatchingButton.js'
+import DownloadButton from './DownloadButton.js'
 
 import throttle from 'react-throttle-render'
 
 const ThrottledChart = throttle(Chart, 200)
 
-const mapStateToProps = ({ dispatch }) => ({
-  dispatch,
+const mapStateToProps = ({ dispatch, participants, pairs, game_page }) => ({
+  dispatch, participants, pairs, game_page
 })
 
 class App extends Component {
@@ -29,6 +30,7 @@ class App extends Component {
   }
 
   render() {
+    const { participants, pairs, game_page } = this.props
     return (
       <div>
         <PageSteps />
@@ -36,6 +38,20 @@ class App extends Component {
         <ThrottledChart />
         <DemandEditor />
         <MatchingButton />
+        <DownloadButton
+          fileName={"spatial_competition.csv"}
+          list={[
+            ["立地ゲーム"],
+            ["実験日", new Date()],
+//            ["登録者数", Object.keys(participants).length],
+            ["グループ数", Object.keys(pairs).length],
+            ["ID"],
+          ].concat(
+//            Object.keys(participants).map(id => [id])
+          )}
+          style={{marginLeft: '2%'}}
+          disabled={game_page != "result"}
+        />
       </div>
     )
   }
