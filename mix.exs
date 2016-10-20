a990aecede8b7529117c2f8e5c5218c2397c79b3
Ryohei HAYASHI <hayashi@xee.jp>
2016-10-20 21:28:27 +0900

    Applied naming rules.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SpatialCompetition.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :spatial_competition,
+    [app: :xee_spatial_competition,
      version: "0.1.0",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
